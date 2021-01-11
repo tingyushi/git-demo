cc6ad6d67e60d2585367dea21da94c0b51c05428
tingyushi <bryanshi498@gmail.com>
2021-01-12 05:20:44 +0800

    How to find GCD

---
+++ gcd.java
@@ -0,0 +1,17 @@
+/*
+ * Description:
+ * Compute the GCD of two non-negative integers p and q.
+ * If q == 0, then the answer is p.
+ * If not, divide p by q and take the remainder r. 
+ * The answer is the GCD of q and r.
+ * */
+
+
+public class gcd {
+	public static int getGCD(int p, int q) {
+		if (q == 0) {
+			return p;
+		}
+		return getGCD(q, p % q);
+	}
+}
